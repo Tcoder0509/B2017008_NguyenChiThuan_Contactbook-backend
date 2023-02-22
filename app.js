e56a8292c406c86626cf83d5b7ec504f2adec7b9
Sovoi-yasuo <95270678+Sovoi-yasuo@users.noreply.github.com>
2023-02-22 11:44:39 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 
+const ApiError = require("./app/api-error");
+
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require('./app/api-error');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    })
+});
 
 app.get("/", (req, res) => {
     res.json({ message: "Wellcome to contact book application."});
